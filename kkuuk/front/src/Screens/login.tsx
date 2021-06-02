722a57250bd0624112ddd44bf56f3782ab2980b5
ahn kyung rok <akrnote97@gmail.com>
2021-06-02 22:08:29 +0900

    feature signup

--- kkuuk/front/src/Screens/login.tsx
+++ kkuuk/front/src/Screens/login.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 type LoginProps = {
 
 }
 
+type memberProps = {
+    num : string;
+    name : string;
+}
+
 function Login({}:LoginProps){
-    //state
+        const [member,setMember] = useState<memberProps>(
+            {
+                num : "",
+                name : "",
+            }
+        );
+
+        const joinmemberHandler = (e:React.ChangeEvent<HTMLInputElement>) =>{
+            setMember({
+                ...member,
+                [e.target.name] : e.target.value,
+            })
+        }
+
+
         return(
             <section className="container">
                 <div>
+                    {/* useEffect => get 으로 회원정보 보여줌.*/ }
                     <h3>업체명</h3>
                     <p>총 회원수</p>
                     <p>총 포인트</p>
                 </div>
                 <div>
                     <h3>회원추가</h3>
-                    <p>전화번호(끝 4자리)<input type="text"/></p>
-                    <p>이름<input type="text"/></p>
+                    <p>전화번호(끝 4자리)<input type="text" onChange={joinmemberHandler} value={member.num} name="num"/></p>
+                    <p>이름<input type="text" onChange={joinmemberHandler} value={member.name} name="name"/></p>
                     <button>추가</button>
                 </div>
                 <div>
--- kkuuk/front/src/Screens/signup.tsx
+++ kkuuk/front/src/Screens/signup.tsx
@@ -1,48 +1,55 @@
 import React, { useState } from 'react';
-import api from "../Api/api";
 import {Link} from "react-router-dom";
+import {signup} from "../Api/api";
 
 
 type signupProps = {
-
+    name : string;
+    call : string;
+    password : string;
 }
 
-function SignUp({}:signupProps){
+function SignUp({name,call,password}:signupProps){
 
 
     //state
-    const input = document.querySelector('input');
+    const [seller,setSeller] = useState<signupProps>({
+        name : "",
+        call : "",
+        password : "",
+    });
+
+    const signupHandler = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        setSeller({
+            ...seller,
+            [e.target.name]:e.target.value,
+        });
+    }
+
+    const passwordCorrectHandler = ()=>{
+        //if(seller.password === )
+    }
+    //유효성검사 추가 중복 등.
 
         return(
             <section className="container">
                 <h1>가게등록</h1>
-                <p>전화번호 : 
-                    <input 
-                        type="text"
-                        
-                    /></p>
-                <p>가게명 : <input type="text"/></p>
-                <p>비밀번호 : <input type="text"/></p>
-                <p>비밀번호확인 : <input type="text"/></p>
+                <p>전화번호 : <input type="text" onChange={signupHandler} value={seller.call} name="call"/></p>
+                <p>상호명 : <input type="text" onChange={signupHandler} value={seller.name} name="name"/></p>
+                <p>비밀번호 : <input type="text" onChange={signupHandler} value={seller.password} name="password"/></p>
+                <p>비밀번호확인 : <input type="text" onChange={passwordCorrectHandler}/></p>  {/*유효성 검사*/}
                 <button
                 onClick={()=>
                     {       
-                        console.log(typeof input);
-                        console.log(input);
-                               api.signUp({
-                                //   name : 
-                                //   call : 
-                                //   password :
-                               }).then((response) => {
-                                   return response.data
-                               }).then((data) => {
-                                   if (data.success) {
-                                        <Link to="/">업체등록</Link>
-                                   }
-                                   else {
-                                       alert('Already register');
-                                   }
-                               }) 
+                        //post
+                        console.log(seller);
+                        signup(seller).then(response=>response.data).then((data)=>{
+                            if(data.success){
+                                <Link to="/"/>
+                            }else{
+
+                            }
+                        })
                     }
                    
                    }>등록</button>
