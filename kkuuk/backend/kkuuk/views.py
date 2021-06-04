25185c57457f9e5d687892909b52b4641bd18dfd
chaeng <cathyshim222@naver.com>
2021-06-04 22:10:58 +0900

    feature#4 kkuuk의 models.py 수정 membership 모델 추가

--- kkuuk/backend/kkuuk/views.py
+++ kkuuk/backend/kkuuk/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets 
+from .serializers import StoreSerializer 
+from .serializers import UserSerializer
+from .serializers import HistorySerializer
+from .models import Store
+from django.http import HttpResponse
 
-# Create your views here.
+class StoreViewSet(viewsets.ModelViewSet): 
+    queryset = Store.objects.all() 
+    serializer_class = StoreSerializer 
+
+def index(request):
+    return HttpResponse("적립 서비스 '꾸욱' 입니다.")
